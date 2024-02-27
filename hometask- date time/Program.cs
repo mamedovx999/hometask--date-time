eab4d9a9f8c2b5c28844c9b62b232ef4f6277546
mamedovx999 <mamedovx999@code.edu.az>
2024-02-28 02:44:53 +0400

    Proje dosyası ekle.

---
+++ hometask- date time/Program.cs
@@ -0,0 +1,32 @@
+﻿
+#region Task1
+//int[] number = new int[] { 34, 567, 89, 54, 45 };
+//for (int i = 0; i < number.Length; i++)
+//{
+//    Console.WriteLine(number[i]);
+//    Array.Reverse(number);
+//}
+//Console.WriteLine("=========");
+//for (int i = 0;i < number.Length; i++)
+//    Console.WriteLine(number[i]);
+#endregion
+#region Task2
+//Console.WriteLine("Dogum tarixinizi daxil edin");
+//var birthday =  DateTime.Parse(Console.ReadLine());
+//var age = DateTime.Now.Year - birthday.Year;
+//Console.WriteLine(age);
+#endregion
+#region Task3
+using System.ComponentModel.Design;
+
+//int year = 2022;
+//if ((year % 400 == 0 || year % 100 != 0) && (year % 4 == 0))
+//{
+//    Console.WriteLine("leap year ");
+//}
+//else
+//{
+//    Console.WriteLine(" leap year deyil");
+//}
+
+#endregion
