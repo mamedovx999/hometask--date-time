0adecb809ecd2c332fd703c7c382a892abd4e3f1
mamedovx999 <mamedovx999@code.edu.az>
2024-02-28 18:17:51 +0400

    "fix add 2"

--- hometask- date time/Program.cs
+++ hometask- date time/Program.cs
@@ -2,20 +2,43 @@
 #region Task1
 //void ReverseNumber(string[] word)
 //{
-//    string[] newArr = new string[word.Length];
+//	string[] newArr = new string[word.Length];
 //	int index = 0;
-//	for (int i = word.Length-1; i >= 0; i--)
+//	for (int i = word.Length - 1; i >= 0; i--)
 //	{
 //		newArr[index] = word[i];
 //		index++;
 //	}
-//    foreach (var item in newArr)
-//    {
-//        Console.WriteLine(item);
-//    }
+//	foreach (var item in newArr)
+//	{
+//		Console.WriteLine(item);
+//	}
 //}
-//string[] stringarr = { "Salam ", "Necesen", "Pb301"};
+//string[] stringarr = { "Salam ", "Necesen", "Pb301" };
 //ReverseNumber(stringarr);
+
+// basqa cur
+void ReverseNumber(string[] word)
+{
+    string[] newArr = new string[0];
+    for (int i = word.Length - 1; i >= 0; i--)
+    {
+        AddItem(ref newArr, word[i]);
+    }
+
+    foreach (var item in newArr)
+    {
+        Console.WriteLine(item);
+    }
+}
+string[] stringarr = { "Salam", "Necesen", "Pb301" };
+ReverseNumber(stringarr);
+
+void AddItem(ref string[] arr, string item)
+{
+    Array.Resize(ref arr, arr.Length + 1);
+    arr[arr.Length - 1] = item;
+}
 #endregion
 #region Task2
 //Console.WriteLine("Dogum tarixinizi daxil edin");
